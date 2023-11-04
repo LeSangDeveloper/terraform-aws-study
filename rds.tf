193ff9a7ba2f7325df7126cec6014e5d353304e8
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-11-04 18:15:30 +0800

    test sleep

--- rds.tf
+++ rds.tf
@@ -9,14 +9,17 @@
 #   parameter_group_name = "default.mysql5.7"
 #   skip_final_snapshot = "true"
 # }
-resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  username             = "foo"
-  password             = "foobarz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
+#resource "aws_db_instance" "default" {
+# allocated_storage    = 5
+# db_name              = "mydb"
+# engine               = "mysql"
+# engine_version       = "5.7"
+# instance_class       = "db.t2.micro"
+# username             = "foo"
+# password             = "foobarz"
+# parameter_group_name = "default.mysql5.7"
+# skip_final_snapshot  = true
+#}
+resource "time_sleep" "wai_30_seconds" {
+  create_duration = "30s"
 }
